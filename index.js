5facf704b27175eb14e9e09e28621eabda9ba6c4
micuat <micuat@gmail.com>
2018-03-05 15:41:54 -0500

    refactoring (not done)

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let port_http = 8080;
 
+var osc_eval = require('./osc_eval');
+
+// if(process.argv[2] !== undefined)
+// blah
+
 let startTime = Date.now();
-console.log("OSC Eval launched at " + startTime);
+console.log("Scenic Eval launched at " + startTime);
 
 // init dependencies
-var osc = require('node-osc');
+//var osc = require('node-osc');
+var midi = require('midi');
+const { exec } = require('child_process');
 
 var express = require('express');
 var app = express();
@@ -18,6 +25,14 @@ process.on('uncaughtException', function(err) {
          console.log(err);
     process.exit(1);
 });
+
+// Set up midi input / output
+var midi_output = new midi.output();
+var midi_input = new midi.input();
+
+// aconnect routing
+exec("aconnect 'RtMidi Output Client':0 'iConnectMIDI2+':2");
+exec("aconnect 'iConnectMIDI2+':2 'RtMidi Input Client':0");
 
 http.listen(port_http, function(){
   console.log('listening on *:' + port_http);
@@ -33,73 +48,20 @@ io.on('connection', function(socket){
   });
   socket.on('command', function(msg){
     console.log('message: ' + msg.IP);
-    let array = [];
-    let latency = [];
-    let numMatched = 0;
-
-    let server = new osc.Server(msg.in_port, '0.0.0.0');
-    let client = new osc.Client(msg.IP, msg.out_port);
-
-    let numPackets = msg.num_packets;
-    let tInterval = 1000.0 / msg.fps; // msec
-
-    for(let i = 0; i < numPackets; i++)
-    {
-      latency.push(NaN);
-      array.push(Math.random().toFixed(8));
-      //array.push(i);
-    }
-
-    for(let i = 0; i < numPackets; i++)
-    {
-      setTimeout(function() {
-        let num = array[i];
-        client.send('/test', i, num, Date.now() - startTime, function (error) {
-          // console.log("sent " + i);
-        });
-      }, tInterval * i);
-    }
-
-    server.on('message', function (msg) {
-      let receivedTime = Date.now() - startTime;
-      if(msg[0] == '/test') {
-        let index = msg[1];
-        let arg = msg[2];
-        let sentTime = msg[3];
-        let str = "";
-        if(arg == array[index]) {
-          str += index + " matched:  " + arg + " == " + array[index];
-          numMatched += 1;
-        }
-        else {
-          str += index + " mismatch: " + arg + " != " + array[index];
-        }
-        str += " latency: " + (receivedTime - sentTime) + " msec";
-        socket.emit('log', str);
-        console.log(str);
-        latency[index] = receivedTime - sentTime;
-      }
-    });
+    let params = {startTime: startTime};
+    let eval = new osc_eval(msg, params);
+    eval.evaluate();
 
     // terminate
     setTimeout(function() {
       // statistics
-      let count = 0;
-      let sum = 0;
-      for(let i = 0; i < array.length; i++) {
-        if(latency[i] != NaN) {
-          sum += latency[i];
-          count++;
-        }
-      }
-      let avg = sum / count;
       let str = "--------";
       socket.emit('log', str);
       console.log(str);
-      str = "correct rate: " + parseFloat(numMatched) / latency.length;
+      str = "correct rate: " + eval.getCorrectRate();
       socket.emit('log', str);
       console.log(str);
-      str = "average latency: " + avg + " msec";
+      str = "average latency: " + eval.getAverageLatency() + " msec";
       socket.emit('log', str);
       console.log(str);
       str = "statistics";
@@ -108,16 +70,7 @@ io.on('connection', function(socket){
       str = "--------";
       socket.emit('log', str);
       console.log(str);
-    }, tInterval * numPackets + 1000);
+    }, eval.tInterval * eval.numPackets + 1000);
   });
 });
 
-// logging
-// var fs = require('fs');
-// var util = require('util');
-// var log_file = fs.createWriteStream('/home/pi/shared/scenesouvertes/raspi_ble_osc/log', {flags: 'w'});
-// var log_stdout = process.stdout;
-// console.log = function(d) {
-//   log_file.write(util.format(d) + '\n');
-//   log_stdout.write(util.format(d) + '\n');
-// }
